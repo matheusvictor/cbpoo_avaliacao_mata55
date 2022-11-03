cb8dde312db925a5c9e88d3c04cdf6a29f69c2ef
Matheus Victor <matheusvictor.salles@gmail.com>
2022-11-03 16:01:26 -0300

    feat/wip: Adiciona métodos acessores

--- src/models/Artigo.java
+++ src/models/Artigo.java
@@ -3,14 +3,14 @@ package models;
 import models.especialista.AutorDeArtigo;
 import models.especialista.RevisorDeArtigo;
 
-import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 import java.util.HashSet;
+import java.time.LocalDate;
 
 public class Artigo {
 
     private static int contador = 1;
+
     protected int identificador;
     protected String titulo;
     protected String resumo;
@@ -22,16 +22,64 @@ public class Artigo {
 
 
     public Artigo(String titulo, String resumo, List<String> palavrasChave,
-                  int quantidadeDePaginas, Date dataSubmissao, HashSet<AutorDeArtigo> autores) {
+                  int quantidadeDePaginas, HashSet<AutorDeArtigo> autores) {
 
         this.identificador = contador;
         this.titulo = titulo;
         this.resumo = resumo;
         this.autores = autores;
+        this.revisores = new HashSet<>();
         this.palavrasChave = palavrasChave;
         this.quantidadeDePaginas = quantidadeDePaginas;
         this.dataSubmissao = LocalDate.now();
 
         contador++;
     }
+
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getResumo() {
+        return resumo;
+    }
+
+    public HashSet<AutorDeArtigo> getAutores() {
+        return autores;
+    }
+
+    public HashSet<RevisorDeArtigo> getRevisores() {
+        return revisores;
+    }
+
+    public List<String> getPalavrasChave() {
+        return palavrasChave;
+    }
+
+    public int getQuantidadeDePaginas() {
+        return quantidadeDePaginas;
+    }
+
+    public LocalDate getDataSubmissao() {
+        return dataSubmissao;
+    }
+
+    @Override
+    public String toString() {
+        return "Artigo{" +
+                "identificador=" + identificador +
+                ", titulo='" + titulo + '\'' +
+                ", resumo='" + resumo + '\'' +
+                ", autores=" + autores +
+                ", revisores=" + revisores +
+                ", palavrasChave=" + palavrasChave +
+                ", quantidadeDePaginas=" + quantidadeDePaginas +
+                ", dataSubmissao=" + dataSubmissao +
+                '}';
+    }
+
 }
