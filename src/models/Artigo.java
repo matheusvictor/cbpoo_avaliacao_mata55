f0fd1643d074f489dbbfd8682eb01f6c7abc7574
Matheus Victor <matheusvictor.salles@gmail.com>
2022-11-08 01:12:23 -0300

    feat: Add métodos de acesso e comparação

--- src/models/Artigo.java
+++ src/models/Artigo.java
@@ -1,37 +1,114 @@
 package models;
 
-import models.especialista.AutorDeArtigo;
-import models.especialista.RevisorDeArtigo;
+import models.especialistas.Autor;
+import models.especialistas.Revisor;
 
 import java.time.LocalDate;
-import java.util.Date;
-import java.util.List;
 import java.util.HashSet;
+import java.util.List;
 
-public class Artigo {
+public class Artigo implements Comparable<Artigo> {
 
     private static int contador = 1;
+
     protected int identificador;
     protected String titulo;
     protected String resumo;
-    protected HashSet<AutorDeArtigo> autores;
-    protected HashSet<RevisorDeArtigo> revisores;
+    protected HashSet<Autor> autores;
+    protected HashSet<Revisor> revisores;
     protected List<String> palavrasChave;
     protected int quantidadeDePaginas;
     protected LocalDate dataSubmissao;
-
+    protected boolean aprovado;
+    protected boolean aguardandoAvaliacao;
 
     public Artigo(String titulo, String resumo, List<String> palavrasChave,
-                  int quantidadeDePaginas, Date dataSubmissao, HashSet<AutorDeArtigo> autores) {
+                  int quantidadeDePaginas) {
 
         this.identificador = contador;
+
         this.titulo = titulo;
         this.resumo = resumo;
-        this.autores = autores;
+        this.autores = new HashSet<>();
+        this.revisores = new HashSet<>();
         this.palavrasChave = palavrasChave;
         this.quantidadeDePaginas = quantidadeDePaginas;
+        this.aprovado = false;
+        this.aguardandoAvaliacao = true;
         this.dataSubmissao = LocalDate.now();
 
         contador++;
     }
+
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getResumo() {
+        return resumo;
+    }
+
+    public void vincularAutor(Autor autor) {
+        this.autores.add(autor);
+    }
+
+    public HashSet<Autor> getAutores() {
+        return autores;
+    }
+
+    public HashSet<Revisor> getRevisores() {
+        return revisores;
+    }
+
+    public List<String> getPalavrasChave() {
+        return palavrasChave;
+    }
+
+    public int getQuantidadeDePaginas() {
+        return quantidadeDePaginas;
+    }
+
+    public void setAguardandoAvaliacao(boolean aguardandoAvaliacao) {
+        this.aguardandoAvaliacao = aguardandoAvaliacao;
+    }
+
+    public boolean isAguardandoAvaliacao() {
+        return aguardandoAvaliacao;
+    }
+
+    public void setAprovado(boolean aprovado) {
+        this.aprovado = aprovado;
+    }
+
+    public boolean isAprovado() {
+        return aprovado;
+    }
+
+    public LocalDate getDataSubmissao() {
+        return dataSubmissao;
+    }
+
+    @Override
+    public String toString() {
+        return "Artigo{" +
+                "identificador=" + identificador +
+                ", titulo='" + titulo + '\'' +
+                ", resumo='" + resumo + '\'' +
+                ", autores=" + autores +
+                ", revisores=" + revisores +
+                ", palavrasChave=" + palavrasChave +
+                ", quantidadeDePaginas=" + quantidadeDePaginas +
+                ", dataSubmissao=" + dataSubmissao +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Artigo o) {
+        return this.getTitulo().compareTo(o.getTitulo());
+    }
+
 }
--- src/models/Participante.java
+++ src/models/Participante.java
@@ -4,16 +4,45 @@ import java.time.LocalDate;
 
 public class Participante extends Pessoa {
 
+    protected String senha;
     protected boolean inscricaoValida;
     protected boolean validacaoPendente;
 
-    public Participante(String cpf, String nome, String senha, LocalDate dataNascimento,
-                        String titulacaoAcademica, String instituicaoDeVinculo) {
-        super(cpf, nome, senha, dataNascimento, titulacaoAcademica, instituicaoDeVinculo);
+    public Participante(String cpf, String nome, String senha,
+                        LocalDate dataNascimento, String titulacaoAcademica, String instituicaoDeVinculo) {
+        super(cpf, nome, dataNascimento, titulacaoAcademica, instituicaoDeVinculo);
+        this.senha = senha;
         this.inscricaoValida = false;
         this.validacaoPendente = true;
     }
 
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 
+    public String getSenha() {
+        return senha;
+    }
+
+    public boolean isInscricaoValida() {
+        return inscricaoValida;
+    }
+
+    public boolean isValidacaoPendente() {
+        return validacaoPendente;
+    }
+
+    @Override
+    public String toString() {
+        return "Participante{" +
+                "inscricaoValida=" + inscricaoValida +
+                ", validacaoPendente=" + validacaoPendente +
+                ", cpf='" + cpf + '\'' +
+                ", nome='" + nome + '\'' +
+                ", dataNascimento=" + dataNascimento +
+                ", titulacaoAcademica='" + titulacaoAcademica + '\'' +
+                ", instituicaoDeVinculo='" + instituicaoDeVinculo + '\'' +
+                '}';
+    }
 
 }
