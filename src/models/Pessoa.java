4c9626dc813ba66b033e852a8f969b6788ae550e
Matheus Victor <matheusvictor.salles@gmail.com>
2022-11-08 00:48:34 -0300

    fix: Modifica atributos e métodos de Pessoa

--- src/models/Pessoa.java
+++ src/models/Pessoa.java
@@ -2,7 +2,7 @@ package models;
 
 import java.time.LocalDate;
 
-public class Pessoa {
+public abstract class Pessoa {
 
     protected String cpf;
     protected String nome;
@@ -11,14 +11,60 @@ public class Pessoa {
     protected String titulacaoAcademica;
     protected String instituicaoDeVinculo;
 
-    public Pessoa(String cpf, String nome, String senha, LocalDate dataNascimento,
+    public Pessoa(String cpf, String nome, LocalDate dataNascimento,
                   String titulacaoAcademica, String instituicaoDeVinculo) {
         this.cpf = cpf;
         this.nome = nome;
-        this.senha = senha;
         this.dataNascimento = dataNascimento;
         this.titulacaoAcademica = titulacaoAcademica;
         this.instituicaoDeVinculo = instituicaoDeVinculo;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setTitulacaoAcademica(String titulacaoAcademica) {
+        this.titulacaoAcademica = titulacaoAcademica;
+    }
+
+    public String getTitulacaoAcademica() {
+        return titulacaoAcademica;
+    }
+
+    public void setInstituicaoDeVinculo(String instituicaoDeVinculo) {
+        this.instituicaoDeVinculo = instituicaoDeVinculo;
+    }
+
+    public String getInstituicaoDeVinculo() {
+        return instituicaoDeVinculo;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "cpf='" + cpf + '\'' +
+                ", nome='" + nome + '\'' +
+                ", dataNascimento=" + dataNascimento +
+                ", titulacaoAcademica='" + titulacaoAcademica + '\'' +
+                ", instituicaoDeVinculo='" + instituicaoDeVinculo + '\'' +
+                '}';
     }
 
 }
