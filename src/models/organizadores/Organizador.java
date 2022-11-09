15dda159498741bda76af099da0693fad33c35e5
Matheus Victor <matheusvictor.salles@gmail.com>
2022-11-09 15:50:16 -0300

    refactor: Remove toString e modifica valores de atributos

--- src/models/organizadores/Organizador.java
+++ src/models/organizadores/Organizador.java
@@ -8,19 +8,8 @@ public class Organizador extends Participante {
 
     public Organizador(String cpf, String nome, String senha, LocalDate dataNascimento, String titulacaoAcademica, String instituicaoDeVinculo) {
         super(cpf, nome, senha, dataNascimento, titulacaoAcademica, instituicaoDeVinculo);
+        setInscricaoValida(true);
+        setValidacaoPendente(false);
     }
 
-    @Override
-    public String toString() {
-        return "Organizador{" +
-                "inscricaoValida=" + inscricaoValida +
-                ", validacaoPendente=" + validacaoPendente +
-                ", cpf='" + cpf + '\'' +
-                ", nome='" + nome + '\'' +
-                ", senha='" + senha + '\'' +
-                ", dataNascimento=" + dataNascimento +
-                ", titulacaoAcademica='" + titulacaoAcademica + '\'' +
-                ", instituicaoDeVinculo='" + instituicaoDeVinculo + '\'' +
-                '}';
-    }
 }
