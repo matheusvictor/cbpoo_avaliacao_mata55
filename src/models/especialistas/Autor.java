69e0c8aa69b0683e34f27ea962b8a1f7c51193a6
Matheus Victor <matheusvictor.salles@gmail.com>
2022-11-13 20:44:27 -0300

    refactor: Altera herança e remove método

--- src/models/especialistas/Autor.java
+++ src/models/especialistas/Autor.java
@@ -10,23 +10,16 @@ public class Autor extends Especialista {
 
     private ArrayList<Artigo> artigosSubmetidos;
 
-    public Autor(String cpf, String nome, LocalDate dataNascimento,
-                 String titulacaoAcademica, String instituicaoDeVinculo, String especialidade) {
-        super(cpf, nome, dataNascimento, titulacaoAcademica, instituicaoDeVinculo, especialidade);
-        this.artigosSubmetidos = new ArrayList<>();
+    public Autor(String cpf, String nome, String senha, LocalDate dataNascimento, String titulacaoAcademica,
+                 String instituicaoDeVinculo, String especialidade, ArrayList<Artigo> artigosSubmetidos) {
+        super(cpf, nome, senha, dataNascimento, titulacaoAcademica, instituicaoDeVinculo, especialidade);
+        this.artigosSubmetidos = artigosSubmetidos;
     }
 
     public Artigo submeterArtigo(String titulo, String resumo, List<String> palavrasChave, int quantidadeDePaginas) {
         Artigo artigo = new Artigo(titulo, resumo, palavrasChave, quantidadeDePaginas);
         this.artigosSubmetidos.add(artigo);
         return artigo;
-    }
-
-    public void vincularCoAutoresAoArtigo(Artigo artigo, String cpf, String nome, LocalDate dataNascimento,
-                                          String titulacaoAcademica, String instituicaoDeVinculo, String especialidade) {
-        Autor coAutor = new Autor(cpf, nome, dataNascimento, titulacaoAcademica, instituicaoDeVinculo, especialidade);
-        if (artigo.getAutores().size() < 5)
-            artigo.vincularAutor(coAutor);
     }
 
 }
