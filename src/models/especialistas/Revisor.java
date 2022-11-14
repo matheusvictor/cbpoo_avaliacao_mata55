2dbe5a75b85518f83eec20f9bf51541a435c029c
Matheus Victor <matheusvictor.salles@gmail.com>
2022-11-13 23:47:08 -0300

    refactor: Modifica inicialização de atributo

--- src/models/especialistas/Revisor.java
+++ src/models/especialistas/Revisor.java
@@ -6,13 +6,11 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Revisor extends Especialista {
+    private ArrayList<Artigo> artigosRevisados = new ArrayList<>();
 
-    private ArrayList<Artigo> artigosRevisados;
-
-    public Revisor(String cpf, String nome, LocalDate dataNascimento, String titulacaoAcademica,
-                   String instituicaoDeVinculo, ArrayList<Artigo> artigosRevisados) {
+    public Revisor(String cpf, String nome, LocalDate dataNascimento,
+                        String titulacaoAcademica, String instituicaoDeVinculo) {
         super(cpf, nome, dataNascimento, titulacaoAcademica, instituicaoDeVinculo);
-        this.artigosRevisados = artigosRevisados;
     }
 
     public void avaliarArtigo(Artigo artigo, String avaliacao) {
